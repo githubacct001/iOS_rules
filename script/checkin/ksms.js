61d67999e22393d7ac22b18d8292425af8db2ec7
Carl Quinn <63450369+githubacct001@users.noreply.github.com>
2024-04-11 14:03:11 +0800

    Update ksms.js

--- script/checkin/ksms.js
+++ script/checkin/ksms.js
@@ -19,27 +19,6 @@ hostname = h5.youzan.com
 
 
 
-/*
-
-#!name = 科丝美诗小程序签到
-#!desc = 脚本支持获取Cookie，兼容QX & Loon & Surge
-#!author = lbqmy2022
-#!icon = https://raw.githubusercontent.com/githubacct001/QuantumultX/master/icon/ksms.png
-#!date = 2024/04/03 13:50
-
-[Script]
-# 定时脚本
-cron "0 8 * * *" script-path=https://raw.githubusercontent.com/githubacct001/iOS_rules/main/script/checkin/ksms.js, timeout=300, tag=签到 - 科丝美诗, img-url=https://raw.githubusercontent.com/githubacct001/QuantumultX/master/icon/ksms.png
-# 获取Cookie
-http-response ^https?:\/\/h5\.youzan\.com\/wscump\/checkin\/get_activity_by_yzuid_v2\.json script-path=https://raw.githubusercontent.com/githubacct001/iOS_rules/main/script/checkin/ksms.js, timeout=300, tag=科丝美诗获取Cookie, img-url=https://raw.githubusercontent.com/githubacct001/QuantumultX/master/icon/ksms.png
-
-[MITM]
-hostname = h5.youzan.com
-
-*/
-
-
-
 const $ = API("科丝美诗小程序", false);
 
 // 尝试获取参数
